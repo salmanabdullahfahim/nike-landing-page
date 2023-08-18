8d1e601540ab1707e8cdf1a19338f3ea6e6d53cf
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-08-18 18:32:12 +0600

    full ui position added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import Hero from "./sections/Hero";
+
+const App = () => {
+  return (
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero></Hero>
+      </section>
+      <section className="padding">Popular Products</section>
+      <section className="padding">Super Quality</section>
+      <section className="padding-x py-10">Services</section>
+      <section className="padding">Special Offer</section>
+      <section className="bg-pale-blue padding">Customer Review</section>
+      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+      <section className="bg-black padding-x padding-t pb-8">Footer</section>
+    </main>
+  );
+};
+
+export default App;
